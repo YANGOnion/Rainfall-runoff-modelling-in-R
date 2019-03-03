88dcb7a9c1be286891ecc734969b436bc7eb5511
YANGOnion <yangwconion@gmail.com>
2019-03-03 16:01:44 +0800

    Update create_point.R
    Fix the bug of creating a point shapefile.

--- wts_extract/create_point.R
+++ wts_extract/create_point.R
@@ -1,8 +1,8 @@
 
 
 library(raster)
-df=data.frame(id=66193,lon=113.45,lat=27.66667)
+df=data.frame(lon=113.45,lat=27.66667)
 p=SpatialPoints(df, proj4string=CRS('+proj=longlat +datum=WGS84'))
 p=SpatialPointsDataFrame(p, data=df)
-shapefile(p, paste('input/point/',i,'.shp',sep=''), overwrite=TRUE)
+shapefile(p, 'input/point/66193.shp', overwrite=TRUE)
 
